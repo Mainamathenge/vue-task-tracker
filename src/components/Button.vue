8c9c1e474f882ec2413b29a2a4f8b2282b72a494
Mainamathenge <mainamathengej@gmail.com>
2023-04-28 23:24:15 +0300

    front-end

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click = "onclick()" :style="{background : color}" class="btn"> {{ text }} </button>
+</template>
+
+<script>
+    export default{
+       // eslint-disable-next-line vue/multi-word-component-names
+       name :  'Button',
+       props : {
+        text :String,
+        color : String
+       },
+       methods : {
+            onclick(){
+                console.log('click')
+            },
+       },
+
+    }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button text = "Add Task " color = "green" />
+    </header>
+</template>
+/* eslint-disable */
+<script>
+import Button from './Button'
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name : 'Header_head',
+    props : {
+        title :{
+            type : String,
+            default : 'hello World',
+        }
+    } ,
+    components : {
+        Button
+    }
+}
+
+</script>
+
+<style scoped>
+    header {
+       display: flex; 
+       justify-content: space-between;
+       align-items: center;
+       margin-bottom: 20 ;
+       
+    }
+</style>
